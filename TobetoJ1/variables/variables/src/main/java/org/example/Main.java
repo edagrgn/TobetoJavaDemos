bcb176e54e1e752f2b592ce9d03d25f33f6236ff
Eda Rumeysa Gürgen <60423013+edagrgn@users.noreply.github.com>
2024-05-28 18:08:04 +0300

    demos

---
+++ TobetoJ1/variables/variables/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //camel casing
+        //reusability
+        int studentNumber =10;
+        String message = "Student number";
+
+        System.out.println(message +":"+studentNumber);
+        System.out.println(message+":"+studentNumber);
+        System.out.println("Student number:"+studentNumber);
+        System.out.println("Student number:"+studentNumber);
+        System.out.println("Student number:"+studentNumber);
+
+    }
+}

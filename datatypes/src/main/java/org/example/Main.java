109df1d3ff470456b1a31a6f8a07bba9d67ffc0e
Eda Rumeysa Gürgen <60423013+edagrgn@users.noreply.github.com>
2024-05-29 11:18:00 +0300

    demos

---
+++ datatypes/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int number=12;
+number=1111111111;
+
+byte num2= 12;
+num2=127;
+num2=-128;
+
+double num3=12.5;
+num3=123;
+num3=123456.6;
+
+
+char character='A';
+String text= "Ankara";
+
+boolean isTrue= false;
+
+
+
+
+
+
+    }
+}

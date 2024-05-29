0a7eb027f0c7f575bca9bea8530f1bd59fcc6494
Eda Rumeysa Gürgen <60423013+edagrgn@users.noreply.github.com>
2024-05-29 13:57:48 +0300

    demos

---
+++ loopDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=0 ;i<=10;i++){
+            System.out.println(i);
+        }
+
+        for(int i=2 ;i<=24;i+=2){
+            System.out.println(i);
+        }
+    }
+}
